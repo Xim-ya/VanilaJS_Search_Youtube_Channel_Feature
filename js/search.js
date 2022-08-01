652547ded786a3f19e8852e6b0fc59bbdaf75846
이해주 <hj_lee_com@naver.com>
2022-08-01 22:08:31 +0900

    feature : search bar ui 및 인터렉션 디자인 구현

--- js/search.js
+++ js/search.js
@@ -1,15 +1,26 @@
-const search = document.querySelector('[data-search]');
-// const searchBar = document.querySelector('[data-search = bar]');
 const searchIcon = document.querySelector('[data-search = icon]');
+const searchBar = document.querySelector('[data-search = bar]');
+const searchInput = document.querySelector('[data-search = input]');
+const clearBtn = document.querySelector('[data-search = clearBtn]');
+
 
 const ACTIVE = 'active';
 
 
 // 원형의 SearchBar을 클릭 했을 때
 const onSearchBarTapHandler = (event) => {
-    search.classList.toggle(ACTIVE);
-
+    event.preventDefault();
+    searchInput.classList.toggle(ACTIVE);
+    searchBar.classList.toggle(ACTIVE);
 };
 
-search.addEventListener('click' , onSearchBarTapHandler);
+const onClearBtnTapHandler = (event) => {
+    event.preventDefault();
+    searchInput.classList.toggle(ACTIVE);
+    searchBar.classList.toggle(ACTIVE);
+};
+
+searchIcon.addEventListener('click' , onSearchBarTapHandler);
+clearBtn.addEventListener('click', onClearBtnTapHandler);
+
 
