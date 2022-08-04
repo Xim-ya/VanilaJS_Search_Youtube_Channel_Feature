52721ea8554e1c18f5620d70a567f75844b88215
이해주 <hj_lee_com@naver.com>
2022-08-04 09:05:37 +0900

    feature : 검색 후 channel list show up UI 구현 및 필요 예외처리 로직 고려

--- js/search.js
+++ js/search.js
@@ -13,7 +13,6 @@ const ACTIVE = 'active'; // Search Bar 애니메이션 Toggle 값.
 const CHANNEL_LOADED = false; // 채널 정보 호출 여부
 
 
-
 // 원형의 SearchBar을 클릭했을 때
 const onSearchBarTapHandler = (event) => {
     event.preventDefault();
@@ -27,32 +26,60 @@ const onClearBtnTapHandler = (event) => {
     searchInput.value = ''; // Input Field 값 초기화
     searchInputContainer.classList.toggle(ACTIVE);
     searchBar.classList.toggle(ACTIVE);
+    channelList.classList.remove('show')
+    channelList.innerHTML = ''; // 검색된 채널 list 초기화
 };
+
 
 // TODO: 필요한 기능인지 확인
 // 'input'에 값 입력 되었을 때
 const onTextInput = (event) => {
     event.preventDefault();
     // console.log(event.target.value);
-};
 
+};
 
 
 // 유튜브 채널 검색
 const searchYoutubeChannel = (event) => {
     event.preventDefault();
-    console.log("AIM");
-    loadYoutubeChannelList();
+    // console.log("AIM");
+    // loadYoutubeChannelList();
+
+
+    insertChannelItem();
+    channelList.classList.add('show');
+
+
 };
+
+
+
+const channelNames = ['침책맨', '오킹', '격과사전', '필'];
 
 const insertChannelItem = () => {
-        
-
+    channelList.innerHTML = ''; // 검색된 채널 list 초기화
+    channelList.classList.toggle(ACTIVE);
+    channelNames.map((ele) => {
+        channelList.insertAdjacentHTML("beforeend",
+            `<li>
+            <div class="channel_img"></div>
+                <span class="channel_name">${ele}</span>
+           </li>`
+        )
+    })
 };
+
+// // 필터링된 배열 데이터를 기준으로 `Square` 컴포넌트 생성
+// contributionList.map((ele) => {
+//     squares.insertAdjacentHTML(
+//         "beforeend",
+//         `<li data-level="${ele.contributionLevel}"></li>`
+//     );
 
 
 // const kakaoSign
-searchIcon.addEventListener('click' , onSearchBarTapHandler);
+searchIcon.addEventListener('click', onSearchBarTapHandler);
 clearBtn.addEventListener('click', onClearBtnTapHandler);
 searchInput.addEventListener('input', onTextInput);
 searchForm.addEventListener('submit', searchYoutubeChannel);
